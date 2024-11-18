f7459701fb44d066031a56e682e90b5b77caf645
notmybussiness <cuisine97@naver.com>
2024-11-18 16:44:40 +0900

    2번째 요구사항 완료

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,12 @@ public class Main {
             System.out.print("명령) ");
             if(scanner.nextLine().equals("종료")){
                 break;
+            } else if(scanner.nextLine().equals("등록")){
+                System.out.print("명언 : ");
+                String WiseSaying = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String WiseWriter = scanner.nextLine();
             }
         }
     }
